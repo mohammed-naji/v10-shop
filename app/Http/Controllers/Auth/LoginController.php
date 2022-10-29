11b18d62ca7a71cd8b9bb48dc97585afb4d6f4e1
Mohammed Naji <m.naji1512@gmail.com>
2022-10-29 17:54:30 +0300

    lec19

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function username()
+    {
+        // dd(request()->all());
+        $type = 'phone';
+        $input = request()->email;
+        if(filter_var($input, FILTER_VALIDATE_EMAIL)) {
+            $type = 'email';
+        }
+
+        if($type == 'phone') {
+            // request()->request->remove('email');
+        }
+
+        request()->request->add([ $type => $input]);
+        // request()->merge([ $type => $input]);
+
+        // dd(request()->all());
+
+        return $type;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
-Route::prefix('admin')->name('admin.')->group(function() {
+Route::prefix('admin')->name('admin.')->middleware('auth', 'check')->group(function() {
     Route::get('/', [AdminController::class, 'index'])->name('index');
 });
 
 Route::get('/', function () {
     return view('welcome');
 })->name('site.home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
